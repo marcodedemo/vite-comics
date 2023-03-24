ccfe5ed6a12b288781699e33640320f3b9960a27
marcodedemo <marco.dedemo@gmail.com>
2023-03-24 14:42:50 +0100

    feat: main + link section (bonus)

--- src/components/AppLinkSection.vue
+++ src/components/AppLinkSection.vue
@@ -4,6 +4,29 @@
     data(){
       return{
 
+        links:[
+            {
+                img: "/public/img/buy-comics-digital-comics.png",
+                text:"digital comics",
+            },
+            {
+                img: "/public/img/buy-comics-merchandise.png",
+                text:"dc merchandise",
+            },
+            {
+                img: "/public/img/buy-comics-subscriptions.png",
+                text:"subscription",
+            },
+            {
+                img: "/public/img/buy-comics-shop-locator.png",
+                text:"comic shop locator",
+            },
+            {
+                img: "/public/img/buy-dc-power-visa.svg",
+                text:"dc power visa",
+            },
+        ]
+
       }
     },
     
@@ -13,7 +36,20 @@
 
 <template>
   <section>
-    ciao
+    <div id="container-centered">
+
+        <div class="card" v-for="(link, index) in links">
+            <div class="logo">
+                <img :src="links[index].img" alt="">
+            </div>
+
+            <div class="text">
+                {{ link.text }}
+            </div>
+        </div>
+
+
+    </div>
   </section>
 </template>
 
@@ -21,5 +57,42 @@
 
 section{
     background-color: #0582f9;
+
+    #container-centered{
+        display: flex;
+        align-items: center;
+
+        max-width: 1200px;
+        margin-left: auto;
+        margin-right: auto;
+
+        height: 200px;
+
+        .card{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 10px;
+
+            width: calc(100% / 5);
+
+            .logo{
+                width: 60px;
+                height: 60px;
+
+                img{
+                    width: 100%;
+                    height: 100%;
+                    object-fit: contain;
+                }
+            }
+
+            .text{
+                text-transform: uppercase;
+                color: white;
+                font-size: .9em;
+            }
+        }
+    }
 }
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,6 +4,8 @@
     data(){
       return{
 
+        mainInnerText: "--> Content goes here <--"
+
       }
     },
     
@@ -13,13 +15,35 @@
 
 <template>
   <main>
-    ciao
-  </main>
+    <div id="container-centered">
+
+        <div id="inner-text">{{ mainInnerText }}</div>
+
+    </div>
+    </main>
 </template>
 
 <style lang="scss" scoped>
 
 main{
-    background-color: black;
+
+    background-color: #1c1c1c;
+    
+    #container-centered{
+        display: flex;
+        align-items: center;
+        
+        height: 150px;
+        max-width: 1200px;
+        margin-left: auto;
+        margin-right: auto;
+        
+        #inner-text{
+            color: white;
+            font-weight: bold;
+            font-size: 2em;
+            
+        }
+    }
 }
 </style>
